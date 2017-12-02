c0f5ff3097c759b4e94558d11646c1bb042877e4
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-12-01 19:05:54 -0800

    Add readme

--- sketch.js
+++ sketch.js
@@ -1,18 +1,47 @@
-/** 
-You always need a setup function in p5.js.
-This gets applied just once upon the initial
-load of your sketch.
-**/
-function setup() {
+var x = 0;
+var y = 0;
+var myCanvasWidth = 800;
+var myCanvasHeight = 800;
 
+function setup() {
+  noFill();
+  createCanvas(myCanvasWidth, myCanvasHeight);
+  makeBackground();
 }
 
-/** 
-You always need a draw function in p5.js.
-Right after setup(), gets executed, this gets called continuously 
-in the background. You can think of this function as 
-your primary animation loop––each "frame" of your sketch.
-**/
-function draw() {
+function makeBackground() {
+  background(50, 100, 50);
+}
 
+function draw() {
+  noFill();
+  ellipse(x, y, 20, 20);
+  x = x + 5;
+
+  var weAreAtTheRightSideOfTheScreen = x > myCanvasWidth;
+  if (weAreAtTheRightSideOfTheScreen) {
+    y = y + 100;
+    x = 0;
+  }
+
+  if ( y > myCanvasHeight ) {
+    noLoop();
+  }
+}
+
+
+function mousePressed() {
+  clear();
+  makeBackground();
+  fill(0, 0, 200);
+  ellipse(mouseX, mouseY, 40, 40);
+
+  // var isNearX = (mouseX > (x - 20)) && (mouseX < (x + 20));
+  // var isNearY = (mouseY > (y - 20)) && (mouseY < (y + 20));
+
+  // if (isNearX && isNearY) {
+  //   textSize(32);
+  //   fill(200, 0, 0);
+  //   text("success! ", 20, 200);
+  // }
 }
